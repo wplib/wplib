09c45f057d0cdbf08eca6aed47377a9fba9add98
Mike Schinkel <mike@newclarity.net>
2015-05-08 23:19:07 -0700

    Implemented WPLib_People::get_person_list(), helper registration and related delegation from WPLib_Module_Base::__callStatic().

--- includes/class-module-base.php
+++ includes/class-module-base.php
@@ -5,4 +5,51 @@
  */
 class WPLib_Module_Base extends WPLib {
 
+	/**
+	 * Delegate calls to a main class if the class has a main class, otherwise delegate to WPLib
+	 *
+	 * @param string $method_name
+	 * @param array $args
+	 *
+	 * @return mixed
+	 */
+	static function __callStatic( $method_name, $args ) {
+
+
+		if ( ! ( $main_class = static::constant( 'MAIN_CLASS' ) ) ) {
+
+			$main_class = preg_replace( '#^(.+)s$#', '$1', get_called_class() );
+
+		}
+
+		if ( ! is_callable( array( $main_class, $method_name ) ) ) {
+
+			$main_class = null;
+
+		} else {
+
+			$reflector = new ReflectionMethod( $main_class, $method_name );
+
+			if ( ! $reflector->isStatic() ) {
+
+				$main_class = null;
+
+			}
+
+		}
+
+		if ( $main_class ) {
+
+			$value = call_user_func_array( array( $main_class, $method_name ), $args );
+
+		} else {
+
+			$value = parent::__callStatic( $method_name, $args );
+
+		}
+
+		return $value;
+
+	}
+
 }
--- modules/people/people.php
+++ modules/people/people.php
@@ -2,10 +2,24 @@
 
 /**
  * Class WPLib_People
+ *
+ * @mixin WPLib_Person
+ * @static WPLib_Person[] get_list()
  */
 class WPLib_People extends WPLib_Module_Base {
 
+	const MAIN_CLASS = 'WPLib_Person';
+
 	static function on_load() {
+
+		self::register_helper( 'WPLib_Person', 'WPLib' );
+		self::register_helper( __CLASS__, 'WPLib' );
+
+	}
+
+	static function get_person_list() {
+
+		return WPLib_Person::get_list( $query, $args );
 
 	}
 
