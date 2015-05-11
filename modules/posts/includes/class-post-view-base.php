abf7b51f6e4968b2cd33c48cdc6ac894b739ec67
Mike Schinkel <mike@newclarity.net>
2015-05-11 00:36:37 -0400

    Hardcode get_title_link() and the_title_link() methods for WPLib_Post_Model_Base.

--- modules/posts/includes/class-post-view-base.php
+++ modules/posts/includes/class-post-view-base.php
@@ -23,4 +23,34 @@ abstract class WPLib_Post_View_Base extends WPLib_View_Base {
 
 	}
 
+	/**
+	 * Returns HTML for the title hyperlinked with the post's URL.
+	 *
+	 * @param array $args
+	 *
+	 * @return string
+	 */
+	function get_title_link( $args = array() ) {
+
+		$model = $this->model();
+
+		$url = $model->url();
+
+		$title = $model->title();
+
+		return WPLib::get_html_link( $url, $title, $args );
+
+	}
+
+	/**
+	 * Echos HTML for the title hyperlinked with the post's URL.
+	 *
+	 * @param array $args
+	 */
+	function the_title_link( $args = array() ) {
+
+		echo wp_kses_post( $this->get_title_link( $args ) );
+
+	}
+
 }
