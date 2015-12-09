61fa58d64646722451fcf0ec9e770f4c18182597
Mike Schinkel <mike@newclarity.net>
2015-12-08 20:15:35 -0500

    0.9.6 - Bug fixes, debugging enhancements

--- wplib.php
+++ wplib.php
@@ -6,7 +6,7 @@
  * Plugin Name: WPLib
  * Plugin URI:  http://wordpress.org/plugins/wplib/
  * Description: A WordPress Website Foundation Library Agency and Internal Corporate Developers
- * Version:     0.9.5
+ * Version:     0.9.6
  * Author:      The WPLib Team
  * Author URI:  http://wplib.org
  * Text Domain: wplib
