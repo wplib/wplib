29bb238f2521e02b2cad4992d97cbb95c07bceb4
Mike Schinkel <mike@newclarity.net>
2015-12-16 06:15:25 -0500

    0.11.4 - Fixed an incompatibility w/PHP 5.5 & nginx on Pantheon.

--- wplib.php
+++ wplib.php
@@ -6,7 +6,7 @@
  * Plugin Name: WPLib
  * Plugin URI:  http://wordpress.org/plugins/wplib/
  * Description: A WordPress Website Foundation Library Agency and Internal Corporate Developers
- * Version:     0.11.3
+ * Version:     0.11.4
  * Author:      The WPLib Team
  * Author URI:  http://wplib.org
  * Text Domain: wplib
@@ -43,7 +43,7 @@
  */
 class WPLib {
 
-	const RECENT_COMMIT = 'e056604'; 
+	const RECENT_COMMIT = 'e056604';
 
 	const PREFIX = 'wplib_';
 	const SHORT_PREFIX = 'wplib_';
