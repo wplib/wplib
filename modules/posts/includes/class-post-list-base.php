ce72c23f79f68052bbd9741e616f034cb5d28921
Mike Schinkel <mikeschinkel@gmail.com>
2016-06-20 02:37:29 -0400

    Introduced some fixes to hopefully address some edge cases.

--- modules/posts/includes/class-post-list-base.php
+++ modules/posts/includes/class-post-list-base.php
@@ -19,7 +19,7 @@ class WPLib_Post_List_Base extends WPLib_List_Base {
 		if ( isset( $posts ) && is_array( $posts ) ) {
 
 			$args = wp_parse_args( $args, array(
-				'list_owner' => 'WPLib_Posts',
+				'list_owner' => false,
 			));
 
 			/**
@@ -27,11 +27,25 @@ class WPLib_Post_List_Base extends WPLib_List_Base {
 			 */
 			$list_owner = $args['list_owner'];
 
-			foreach ( $posts as $index => $post ) {
+			if ( ! $list_owner ) {
 
-				$posts[ $index ] = $list_owner::make_new_item( $post, $args );
+				foreach ($posts as $index => $post) {
+
+					$posts[$index] = $list_owner::make_new_item( $post, $args );
+
+				}
+
+			} else {
+
+				foreach ($posts as $index => $post) {
+
+					$list_owner = $args['list_owner'] = WPLib_Posts::get_post_type_class( $post->post_type );
+					$posts[$index] = $list_owner::make_new_item( $post, $args );
+
+				}
 
 			}
+
 
 		}
 
